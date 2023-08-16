0b3d1cb9f462ec157907c57bd0c23060b220505f
minabe <minabe@student.42tokyo.jp>
2023-08-16 15:54:27 +0900

    fix: 出力修正

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,18 +6,25 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 21:56:16 by minabe            #+#    #+#             */
-/*   Updated: 2023/08/11 23:00:08 by minabe           ###   ########.fr       */
+/*   Updated: 2023/08/16 15:52:53 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 
-int	main()
+int	main(int argc, char **argv)
 {
 	std::string	str;
 
-	std::cin >> str;
+	if (argc == 1)
+	{
+		str = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		std::cout << str << std::endl;
+		return (0);
+	}
+	for (int i = 1; i < argc; i++)
+		str += argv[i];
 	std::transform(str.begin(), str.end(), str.begin(), ::toupper);
 	std::cout << str << std::endl;
 	return (0);
