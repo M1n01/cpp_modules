e33e7112980164621fc62d7731afb7af1b5cbfb8
M1n01 <minato.cat.love@gmail.com>
2023-08-26 18:44:56 +0900

    add 00/ex01

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 21:56:16 by minabe            #+#    #+#             */
-/*   Updated: 2023/08/16 15:52:53 by minabe           ###   ########.fr       */
+/*   Updated: 2023/08/19 22:04:08 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,13 @@ int	main(int argc, char **argv)
 	std::string	str;
 
 	if (argc == 1)
-	{
 		str = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-		std::cout << str << std::endl;
-		return (0);
+	else
+	{
+		for (int i = 1; i < argc; i++)
+			str += argv[i];
+		std::transform(str.begin(), str.end(), str.begin(), ::toupper);
 	}
-	for (int i = 1; i < argc; i++)
-		str += argv[i];
-	std::transform(str.begin(), str.end(), str.begin(), ::toupper);
 	std::cout << str << std::endl;
 	return (0);
 }
