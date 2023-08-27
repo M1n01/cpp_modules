d514c4ea9e2c3b60d2bb64cecd827830aec3b9ca
M1n01 <minato.cat.love@gmail.com>
2023-08-27 17:10:06 +0900

    aaa

--- 00/ex01/phonebook.hpp
+++ 00/ex01/phonebook.hpp
@@ -6,21 +6,17 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 16:19:30 by minabe            #+#    #+#             */
-/*   Updated: 2023/08/26 20:03:46 by minabe           ###   ########.fr       */
+/*   Updated: 2023/08/16 17:17:51 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-
-# include <iostream>
 
 class Phonebook
 {
 	private:
 		Contact		_contacts[8];
 		int			_index;
-		void		_add();
-		void		_search();
 };
 
 class Contact
