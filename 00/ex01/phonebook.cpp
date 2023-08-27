0dd8452006eed3ac378d98ba51c7a5be24f04974
minabe <minabe@student.42tokyo.jp>
2023-08-27 16:41:06 +0900

    #2 fix: なおった

--- 00/ex01/phonebook.cpp
+++ 00/ex01/phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 16:19:32 by minabe            #+#    #+#             */
-/*   Updated: 2023/08/27 15:48:40 by minabe           ###   ########.fr       */
+/*   Updated: 2023/08/27 16:02:28 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,7 @@
 
 void	add_contact(Phonebook phonebook)
 {
-	std::string	first_name, last_name, nickname, phone_number, darkest_secret;
-
-	std::cin >> first_name >> last_name >> nickname >> phone_number >> darkest_secret;
-	phonebook._add(first_name, last_name, nickname, phone_number, darkest_secret);
+	phonebook._add();
 }
 
 void	search_contact(Phonebook phonebook)
@@ -32,7 +29,7 @@ int	main()
 
 	while (true)
 	{
-		std::cout << "Please " << std::endl;
+		std::cout << "Please command: ";
 		std::cin >> cmd;
 		if (cmd == "ADD")
 			add_contact(phonebook);
--- 00/ex01/phonebook.hpp
+++ 00/ex01/phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 16:19:30 by minabe            #+#    #+#             */
-/*   Updated: 2023/08/27 15:48:02 by minabe           ###   ########.fr       */
+/*   Updated: 2023/08/27 16:39:53 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,20 +26,33 @@ class Contact
 
 class Phonebook {
 	public:
-		void		_add(std::string first_name, std::string last_name, std::string nickname, std::string phone_number, std::string darkest_secret);
+		void		_add();
 		void		_search();
 	private:
 		Contact		_contacts[8];
-		int			_index;
+		// int			_index;
 };
 
-void	Phonebook::_add(std::string first_name, std::string last_name, std::string nickname, std::string phone_number, std::string darkest_secret)
+void	Phonebook::_add()
 {
-	_contacts[_index]._first_name = first_name;
-	_contacts[_index]._last_name = last_name;
-	_contacts[_index]._nickname = nickname;
-	_contacts[_index]._phone_number = phone_number;
-	_contacts[_index]._darkest_secret = darkest_secret;
+	std::string	first_name, last_name, nickname, phone_number, darkest_secret;
+
+	std::cout << "Please enter the contact you wish to add." << std::endl;
+	std::cout << "First name: ";
+	std::cin >> first_name;
+	_contacts[0]._first_name = first_name;
+	std::cout << "Last name: ";
+	std::cin >> last_name;
+	_contacts[0]._last_name = last_name;
+	std::cout << "Nickname: ";
+	std::cin >> nickname;
+	_contacts[0]._nickname = nickname;
+	std::cout << "Phone number: ";
+	std::cin >> phone_number;
+	_contacts[0]._phone_number = phone_number;
+	std::cout << "darkest_secret: ";
+	std::cin >> darkest_secret;
+	_contacts[0]._darkest_secret = darkest_secret;
 }
 
 void	Phonebook::_search()
