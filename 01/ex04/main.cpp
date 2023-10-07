80f9d5423d3e3a9b602966da382d6fdfce2503e6
M1n01 <minato.cat.love@gmail.com>
2023-10-07 18:32:29 +0900

    feat: fileのコピー

--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -6,13 +6,48 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 20:47:47 by minabe            #+#    #+#             */
-/*   Updated: 2023/10/07 17:10:22 by minabe           ###   ########.fr       */
+/*   Updated: 2023/10/07 18:26:22 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
+#include <fstream>
 
-int	main(void)
+int	main(int argc, char **argv)
 {
+	std::string		filename;
+	std::string		s1;
+	std::string		s2;
+	std::string		line;
+	std::string		replace;
+
+	if (argc == 4)
+	{
+		filename = argv[1];
+		s1 = argv[2];
+		s2 = argv[3];
+		replace = filename + ".replace";
+		std::ifstream	inputFile(filename);
+		if (!inputFile)
+		{
+			std::cerr << "Error: " << filename << " could not be opened." << std::endl;
+			return (EXIT_FAILURE);
+		}
+		std::ofstream	outputFile(replace);
+		if (!outputFile)
+		{
+			std::cerr << "Error: " << replace << " could not be opened." << std::endl;
+			return (EXIT_FAILURE);
+		}
+		while (std::getline(inputFile, line))
+		{
+			// while (line.find(s1) != std::string::npos)
+				/* replace */
+			outputFile << line << std::endl;
+		}
+		inputFile.close();
+		outputFile.close();
+	}
 	return (0);
 }
