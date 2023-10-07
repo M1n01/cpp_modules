060ddf3e57e39ce15cec1fc732a5592513c4eab2
M1n01 <minato.cat.love@gmail.com>
2023-10-07 16:02:50 +0900

    feat: ex02

---
+++ 01/ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 20:47:47 by minabe            #+#    #+#             */
+/*   Updated: 2023/10/07 16:02:08 by minabe           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
+
+	std::cout << "Address of string variable: " << &str << std::endl;
+	std::cout << "Address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << "------" << std::endl;
+
+	std::cout << "Value of string variable: " << str << std::endl;
+	std::cout << "Value pointed by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed by stringREF: " << stringREF << std::endl;
+	return (0);
+}
