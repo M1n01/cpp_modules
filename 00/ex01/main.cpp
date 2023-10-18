71a0c2b434aca54e52f7a48dc5aa95c148a2935d
M1n01 <minato.cat.love@gmail.com>
2023-10-18 15:18:03 +0900

    fix: 入力欄調整

--- 00/ex01/main.cpp
+++ 00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 10:14:07 by minabe            #+#    #+#             */
-/*   Updated: 2023/10/18 15:09:32 by minabe           ###   ########.fr       */
+/*   Updated: 2023/10/18 15:17:29 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,19 +69,19 @@ static void	inputContact(Contact &contact)
 	std::string	input;
 
 	std::cout << "======= Enter Contact =======\n";
-	std::cout << "Enter a first name: ";
+	std::cout << "[First name] => ";
 	getline(std::cin, input);
 	contact.setFirstName(input);
-	std::cout << "Enter a last name: ";
+	std::cout << "[Last name] => ";
 	getline(std::cin, input);
 	contact.setLastName(input);
-	std::cout << "Enter a nickname: ";
+	std::cout << "[Nickname] => ";
 	getline(std::cin, input);
 	contact.setNickname(input);
-	std::cout << "Enter a phone number: ";
+	std::cout << "[Phone number] => ";
 	getline(std::cin, input);
 	contact.setPhoneNumber(input);
-	std::cout << "Enter a darkest secret: ";
+	std::cout << "[Darkest secret] => ";
 	getline(std::cin, input);
 	contact.setDarkestSecret(input);
 	std::cout << "=============================\n";
