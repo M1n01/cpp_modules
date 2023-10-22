0a3880c2e81d4b435f7f07b87b775329673fc410
M1n01 <minato.cat.love@gmail.com>
2023-10-22 18:05:47 +0900

    fix: 微修正

--- 03/ex01/main.cpp
+++ 03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 18:22:18 by minabe            #+#    #+#             */
-/*   Updated: 2023/10/22 16:54:09 by minabe           ###   ########.fr       */
+/*   Updated: 2023/10/22 18:02:35 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,18 @@ int	main(void)
 
 	scavTrap.attack("うごくせきぞう");
 	scavTrap.takeDamage(10);
+	scavTrap.beRepaired(REPAIR);
 	scavTrap.guardGate();
 
 	scavTrap.attack("キラーマシン");
-	scavTrap.takeDamage(100);
+	scavTrap.takeDamage(99);
 	scavTrap.beRepaired(REPAIR);
 	scavTrap.guardGate();
+
+	scavTrap.attack("ゲマ");
+	scavTrap.takeDamage(100000000);
+	scavTrap.beRepaired(REPAIR);
+
 	return (0);
 }
 
