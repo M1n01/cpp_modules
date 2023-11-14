20661840416ef4442aeeab1b4d7687d5f6fe9525
Minato Abe <m.abe.howcollect@gmail.com>
2023-11-14 20:36:21 +0900

    fix: 01/ex02 文字列出力追加

--- 01/ex02/src/main.cpp
+++ 01/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 20:47:47 by minabe            #+#    #+#             */
-/*   Updated: 2023/11/09 20:52:50 by minabe           ###   ########.fr       */
+/*   Updated: 2023/11/14 20:34:25 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ int	main(void)
 	std::string	str = "HI THIS IS BRAIN";
 	std::string	*stringPTR = &str;
 	std::string	&stringREF = str;
+
+	std::cout << "=======ORIGINAL=======" << std::endl;
+	std::cout << str << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
 
 	std::cout << "=======ADDRESS=======" << std::endl;
 	std::cout << "Address of string variable: " << &str << std::endl;
