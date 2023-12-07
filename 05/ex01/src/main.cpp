59773cb366cf83f8605e39a639182db82333b7d2
Minato Abe <m.abe.howcollect@gmail.com>
2023-12-07 12:11:24 +0900

    fix: 05/ex01 formテスト修正

--- 05/ex01/src/main.cpp
+++ 05/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 21:03:38 by minabe            #+#    #+#             */
-/*   Updated: 2023/12/07 11:18:20 by minabe           ###   ########.fr       */
+/*   Updated: 2023/12/07 11:25:16 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,30 @@
 
 int	main(void)
 {
-	Form	form("Form", 42, 42);
-	std::cout << form << std::endl;
-	Form	hardForm("HardForm", 10, 42);
-	std::cout << hardForm << std::endl;
-
 	std::cout << "\n====NORMAL TEST====" << std::endl;
 	try
 	{
-		// default constructor
+		Form	form("Form", 42, 42);
+		std::cout << form << std::endl;
+		Form	confidentialForm("ConfidentialForm", 10, 42);
+		std::cout << confidentialForm << std::endl;
+
 		Bureaucrat	normal("Normal", 42);
 		std::cout << normal << std::endl;
+		Bureaucrat	normal2("Normal2", 42);
+		std::cout << normal2 << std::endl;
+
 		normal.signForm(form);
 		std::cout << form << std::endl;
-		normal.signForm(hardForm);
-		std::cout << hardForm << std::endl;
+		normal.signForm(confidentialForm);
+		std::cout << confidentialForm << std::endl;
+
+		normal2.signForm(form);
+		std::cout << form << std::endl;
+
+		std::cout << "\n====ERROR TEST====" << std::endl;
+		Form	errorForm("ErrorForm", 0, 42);
+		std::cout << errorForm << std::endl;
 	}
 	catch(const std::exception& e)
 	{
@@ -39,10 +48,15 @@ int	main(void)
 	std::cout << "\n====LOWEST TEST====" << std::endl;
 	try
 	{
+		Form	form("Form", 42, 42);
+		std::cout << form << std::endl;
+		Form	confidentialForm("ConfidentialForm", 10, 42);
+		std::cout << confidentialForm << std::endl;
+
 		Bureaucrat	lowest("Lowest", 150);
 		std::cout << lowest << std::endl;
-		lowest.signForm(hardForm);
-		std::cout << hardForm << std::endl;
+		lowest.signForm(confidentialForm);
+		std::cout << confidentialForm << std::endl;
 	}
 	catch(const std::exception& e)
 	{
@@ -52,10 +66,15 @@ int	main(void)
 	std::cout << "\n====HIGHEST TEST====" << std::endl;
 	try
 	{
+		Form	form("Form", 42, 42);
+		std::cout << form << std::endl;
+		Form	confidentialForm("ConfidentialForm", 10, 42);
+		std::cout << confidentialForm << std::endl;
+
 		Bureaucrat	highest("Highest", 1);
 		std::cout << highest << std::endl;
-		highest.signForm(hardForm);
-		std::cout << hardForm << std::endl;
+		highest.signForm(confidentialForm);
+		std::cout << confidentialForm << std::endl;
 		highest.signForm(form);
 		std::cout << form << std::endl;
 	}
