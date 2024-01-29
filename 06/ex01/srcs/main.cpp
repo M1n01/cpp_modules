475f36bfa0d265daeb104122cd9ad0917a4930b4
Minato Abe <m.abe.howcollect@gmail.com>
2024-01-29 22:34:24 +0900

    first commit

---
+++ 06/ex01/srcs/main.cpp
@@ -0,0 +1,12 @@
+
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cout << "Usage: ./convert [value]" << std::endl;
+        return (1);
+    }
+
+    return (0);
+}
---
+++ 06/ex02/srcs/main.cpp
@@ -0,0 +1,11 @@
+
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cout << "Usage: ./convert [value]" << std::endl;
+        return (1);
+    }
+    return (0);
+}
