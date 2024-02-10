692cecf4db85aabe15a519892a98ee0f12472c93
M1n01 <minato.cat.love@gmail.com>
2024-02-10 18:11:23 +0900

    add: 対象のクラスと構造体

--- 06/ex01/srcs/main.cpp
+++ 06/ex01/srcs/main.cpp
@@ -1,12 +1,32 @@
+#include "../includes/Serializer.hpp"
+#include <iostream>
 
-
-int main(int argc, char *argv[])
+int main()
 {
-    if (argc != 2)
+    // Dataオブジェクトの作成
+    Data originalData;
+    originalData.value = 123; // 値の設定
+
+    // シリアライズ
+    uintptr_t serializedData = Serializer::serialize(&originalData);
+
+    // デシリアライズ
+    Data *deserializedData = Serializer::deserialize(serializedData);
+
+    // 結果の表示
+    std::cout << "Original Data Address: " << &originalData << std::endl;
+    std::cout << "Deserialized Data Address: " << deserializedData << std::endl;
+    std::cout << "Deserialized Data Value: " << deserializedData->value << std::endl;
+
+    // ポインタが同じかどうかの確認
+    if (&originalData == deserializedData)
     {
-        std::cout << "Usage: ./convert [value]" << std::endl;
-        return (1);
+        std::cout << "Success: Original and deserialized pointers are the same." << std::endl;
+    }
+    else
+    {
+        std::cout << "Error: Pointers do not match." << std::endl;
     }
 
-    return (0);
+    return 0;
 }
